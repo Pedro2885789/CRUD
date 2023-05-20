c23a42e850561d6ff89f700d30e899f6655305be
Pedro <ped.ooliveiraa@gmail.com>
2023-05-20 20:30:26 -0300

    Implementado Classe repositorio com as operações

---
+++ CRUD/Repositorio/RepositorioListaSingleton.cs
@@ -0,0 +1,35 @@
+﻿namespace CRUD.Repositorio
+{
+    internal class RepositorioListaSingleton : IRepositorio
+    {
+        protected List<Peca> ListaDePeca = Singleton.Instancia()._listaDePecas;
+
+        public List<Peca> ObterTodos()
+        {
+            return ListaDePeca.ToList();
+        }
+        public Peca ObterPorId(int id)
+        {
+            return ListaDePeca.FirstOrDefault(x => x.Id == id)
+                ?? throw new Exception($"Peça não encontrada com id:[{id}]");
+        }
+        public void Adicionar(Peca peca)
+        {
+            ListaDePeca.Add(peca);
+        }
+        public void Editar(int id, Peca pecaEditada)
+        {
+            var pecaAMudar = ObterPorId(id);
+            var index = ListaDePeca.IndexOf(pecaAMudar);
+            
+            ListaDePeca[index] = index != -1 ?
+                pecaEditada : throw new Exception($"Peça não encontrada com id [{id}]");
+
+        }
+        public void Remover(int id)
+        {
+            var pecaSelecionada = ObterPorId(id);
+            ListaDePeca.Remove(pecaSelecionada);
+        }
+    }
+}
