fb95103ef694969c024aae34f12b06125c811a04
Pedro <ped.ooliveiraa@gmail.com>
2023-05-20 23:36:11 -0300

    Quase acabando a migração

---
+++ CRUD/20220520_AdicionaTabelaPeca.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace CRUD
+{
+    [Migration(20220520232800)]
+    internal class _20220520_AdicionaTabelaPeca : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Peca")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Nome").AsString()
+                .WithColumn("Categoria").AsString()
+                .WithColumn("Descricao").AsString()
+                .WithColumn("Estoque").AsInt64()
+                .WithColumn("DataDeFabricacao").AsDateTime();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Peca");
+        }
+
+    }
+}
--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -1,17 +1,38 @@
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace CRUD
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new ControleDePecas());
+            using (var serviceProvider = CreateServices())
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+
         }
+
+        public static ServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString("conexaoBD")
+                    .ScanIn(typeof(_20220520_AdicionaTabelaPeca).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
+        }
+
     }
 }
